018fd73dc119594e40d5e69ae5ad08375783a044
iyert <iyert@moravian.edu>
2021-04-27 15:46:41 -0400

    Added BookmarkTypeConverter. Added getValue() to ImageStatus and VideoStatus.

--- app/src/main/java/edu/moravian/csci299/bookmarks/ImageData.java
+++ app/src/main/java/edu/moravian/csci299/bookmarks/ImageData.java
@@ -1,9 +1,17 @@
 package edu.moravian.csci299.bookmarks;
 
-public class ImageData {
+public class ImageData implements java.io.Serializable {
     public String source;
     public int width;
     public int height;
     public String credit;
     public String caption;
+
+    public ImageData(String source, int width, int height, String credit, String caption) {
+        this.source = source;
+        this.width = width;
+        this.height = height;
+        this.credit = credit;
+        this.caption = caption;
+    }
 }
--- app/src/main/java/edu/moravian/csci299/bookmarks/MainActivity.java
+++ app/src/main/java/edu/moravian/csci299/bookmarks/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import edu.moravian.csci299.bookmark.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
