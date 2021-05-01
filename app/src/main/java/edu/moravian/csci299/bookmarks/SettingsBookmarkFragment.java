a531af1cb1bbb529ed3ef159c485ce7ace5c4900
Isaac Wood <woodi@moravian.edu>
2021-05-01 18:03:15 -0400

    made some fixes

--- app/src/main/java/edu/moravian/csci299/bookmarks/SettingsBookmarkFragment.java
+++ app/src/main/java/edu/moravian/csci299/bookmarks/SettingsBookmarkFragment.java
@@ -1,5 +1,10 @@
 package edu.moravian.csci299.bookmarks;
 
+import androidx.fragment.app.Fragment;
+
 public class SettingsBookmarkFragment {
 
+    public static Fragment newInstance() {
+        return null;
+    }
 }
--- app/src/main/java/edu/moravian/csci299/bookmarks/WebViewFragment.java
+++ app/src/main/java/edu/moravian/csci299/bookmarks/WebViewFragment.java
@@ -24,7 +24,7 @@ public class WebViewFragment extends Fragment {
     private String url;
 
     public static Fragment newInstance() {
-
+        return null;
     }
 
     @Override
